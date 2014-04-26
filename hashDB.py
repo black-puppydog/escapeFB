0094451ec4db34b3a924c80a388daf6b9d3409d8
Daan Wynen <black.puppydog@gmx.de>
2014-04-26 14:25:00 +0200

    Fix stupid pasting error. omg... also make the lambda for parallelization less awful.

--- hashDB.py
+++ hashDB.py
@@ -10,6 +10,7 @@ import traceback
 from concurrent.futures import ThreadPoolExecutor
 import imagehash
 
+WORKER_THREADS = 4
 
 COMMON_IMAGE_PATTERNS = ["*.jpg", "*.JPG", "*.png", "*.PNG"]
 
@@ -36,14 +37,14 @@ def image_descriptor(image_path, prior=None):
                   'aHash': str(imagehash.average_hash(img)),
                   'pHash': str(imagehash.phash(img)),
                   'dHash': str(imagehash.dhash(img)),
-                  }
+        }
         return result
 
     changed = prior['modified'] < mtime
     img = Image.open(image_path)
 
     if changed or not prior["width"]:
-        prior["width"] = img.size[0 ]
+        prior["width"] = img.size[0]
     if changed or not prior["height"]:
         prior["height"] = img.size[1]
 
@@ -67,11 +68,6 @@ def write_dict_to_json(data, filename):
         json.dump(data, f, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': '))
 
 
-def _descriptor(root_path, images):
-    '''return tuple containing the index, filename, a flag that is true if there was prior data, and the actual descriptor'''
-    return lambda t: (t[0], t[1] in images, t[1], image_descriptor(os.path.join(root_path, t[1]), images.get(t[1])))
-
-
 def build_image_db(root_path, db_filename, patterns=COMMON_IMAGE_PATTERNS, resume=True, save_every=600):
     if resume:
         try:
@@ -84,7 +80,7 @@ def build_image_db(root_path, db_filename, patterns=COMMON_IMAGE_PATTERNS, resum
 
             # remove images that have been removed from the library
             img = data["images"]
-            bad=[]
+            bad = []
             for fname in img.keys():
                 if not os.path.isfile(os.path.join(root_path, fname)):
                     bad.append(fname)
@@ -107,7 +103,7 @@ def build_image_db(root_path, db_filename, patterns=COMMON_IMAGE_PATTERNS, resum
         time_finished = datetime.now()
         time_taken = time_finished - time_started
 
-        if(data["finished"]):
+        if (data["finished"]):
             print("{0}: Scan finished, took {1}".format(time_finished, time_taken))
         else:
             print("{0}: Saving {1} intermediate results after {2}"
@@ -116,7 +112,7 @@ def build_image_db(root_path, db_filename, patterns=COMMON_IMAGE_PATTERNS, resum
             "scan_start": str(time_started),
             "scan_end": str(time_finished),
             "scan_duration": str(time_taken),
-            }
+        }
 
         print("{0}: Writing resulting database to {1}".format(datetime.now(), db_filename))
         write_dict_to_json(data, db_filename)
@@ -127,17 +123,38 @@ def build_image_db(root_path, db_filename, patterns=COMMON_IMAGE_PATTERNS, resum
     images_total = len(filenames)
     images = data["images"]
     images_prior = len(data["images"])
-    print("{0}: Found {1} images ({2} new)".format(datetime.now(), images_total, images_total-images_prior))
+    print("{0}: Found {1} images ({2} new)".format(datetime.now(), images_total, images_total - images_prior))
 
     data["finished"] = False
     data["successful"] = True
 
     last_checkpoint = 0
 
-    processed=0
+    def descriptor_helper(t):
+        """return tuple containing the index, a flag that is true if
+        there was prior data, filename, and the actual descriptor"""
+
+        idx, filename_rel = t
+
+        result = {'idx': idx, # index of the image
+                  'had_prior': filename_rel in images, # True iff we had prior information on the image
+                  'filename': filename_rel, # relative file name
+                  'descriptor': image_descriptor(os.path.join(root_path, filename_rel), images.get(filename_rel)) # the actual descriptor
+        }
+
+        return result
+
+    processed = 0
+    time_spent = timedelta(0)
     try:
-        with ThreadPoolExecutor(4) as executor:
-            for (idx, had_prior, filename_rel, desc) in executor.map(_descriptor(root_path, images), enumerate(filenames)):
+        with ThreadPoolExecutor(WORKER_THREADS) as executor:
+            for datapoint in executor.map(descriptor_helper, enumerate(filenames)):
+
+                filename_rel = datapoint["filename"]
+                desc = datapoint["descriptor"]
+                idx = datapoint["idx"]
+                had_prior = datapoint["had_prior"]
+
                 images[filename_rel] = desc
 
                 now = datetime.now()
@@ -146,9 +163,9 @@ def build_image_db(root_path, db_filename, patterns=COMMON_IMAGE_PATTERNS, resum
                 seconds_so_far = time_so_far.total_seconds()
 
                 # save temporary progress, just to be sure
-                if int(seconds_so_far/save_every) > last_checkpoint:
+                if int(seconds_so_far / save_every) > last_checkpoint:
                     write_result()
-                    last_checkpoint = int(seconds_so_far/save_every)
+                    last_checkpoint = int(seconds_so_far / save_every)
 
                 # if this image had prior information then it cannot be counted as an advantage as of now
                 if had_prior:
@@ -157,7 +174,7 @@ def build_image_db(root_path, db_filename, patterns=COMMON_IMAGE_PATTERNS, resum
                     processed += 1
 
                 images_left = images_total - images_prior - processed
-                processing_time_avg = seconds_so_far/float(processed) if processed != 0 else 0
+                processing_time_avg = seconds_so_far / float(processed) if processed != 0 else 0
                 eta = timedelta(seconds=images_left * processing_time_avg)
                 print("{0}: {1:.2f}% ({2} / {3}) ETA: {4} File: {5}"
                       .format(now, done * 100, idx + 1, images_total, eta, filename_rel))
@@ -182,9 +199,9 @@ def build_image_db(root_path, db_filename, patterns=COMMON_IMAGE_PATTERNS, resum
     print("FINISHED\n")
 
 
-if __name__ == "Write results to disk peridically to prevent data loss in case of problems.__main__":
-
+if __name__ == "__main__":
     root_path = sys.argv[1]
     db_filename = sys.argv[2]
 
+    print("Starting database build...")
     build_image_db(root_path, db_filename)
